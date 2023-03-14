0e01f79ad4e2a02f0031ef773a927f34887d4836
Mert Gökduman <mert.gokduman@paratica.com>
2023-03-14 20:27:52 +0300

    Initial Commit

---
+++ RickAndMorty-Combine/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMorty-Combine
+//
+//  Created by Mert Gökduman on 14.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
